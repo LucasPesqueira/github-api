1a24f37312fb47f1428d5cfbd7f7d71902d83f68
Lucas Pesqueira <lucas@petbooking.com.br>
2019-10-27 22:43:51 -0300

    Add route and controller action

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,2 +1,5 @@
 class RepositoriesController < ApplicationController
+  def index
+    render json: SearchRepositoryService.new.search(params)
+  end
 end
--- app/services/search_repository_service.rb
+++ app/services/search_repository_service.rb
@@ -1,6 +1,16 @@
 class SearchRepositoryService
   def search(params = {})
-    Repository.search()
+    @name = params['name'].present? ? params['name'] : '*'
+    @order = params['order'].present? ? params['order'] : 'desc'
+    @limit = params['limit'].present? ? params['limit'] : '5'
+    @language = params['language']
 
+    Repository.search(@name, where: where_params, order: {stargazers_count: @order}, load: false).results.first(@limit.to_i)
+  end
+
+  private
+
+  def where_params
+    return { language: @language } if @language.present?
   end
 end
